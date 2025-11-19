2d64e5e9cd6606321014a5a55d513c2b104d06b8
jymeng18 <aizeuyn@gmail.com>
2025-11-18 21:39:38 -0800

    week12 notes

---
+++ cmpt225-week12/UnionFind/UnionFindTicket.java
@@ -0,0 +1,9 @@
+
+/**
+ * An empty interface that provides the user of UnionFind a ticket for the element
+ * but does not allow access to the data 
+ *
+ */
+public interface UnionFindTicket {
+
+}
